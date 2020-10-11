9418e48bdf13153c4e4b66e7ca51175d950b7a92
Jacob Krueger <jmk4u14@gmail.com>
2020-10-11 18:02:06 -0400

    Added .js file. Created card on index. Created the start of the quiz.

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById("start")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    console.log("hi")
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
